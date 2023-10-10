9df89c5b6a4bf8f9653bcaf9acb33a2703c1883f
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-10-10 14:46:04 +0200

    Update playerdirectory.php

--- inc/plugins/playerdirectory.php
+++ inc/plugins/playerdirectory.php
@@ -6769,7 +6769,7 @@ function playerdirectory_build_statistics($userids_string){
                             // Zählen wie viel von den Charakteren diesen Wert angegeben haben
                             $userdata_query = $db->query("SELECT * FROM ".TABLE_PREFIX."userfields
                             WHERE ufid IN (".$userids_string.")
-                            AND (concat(',',fid".$field.",'\n') LIKE '%,".$option."\n%')
+                            AND (concat('\n',fid".$field.",'\n') LIKE '%\n".$option."\n%')
                             ");
                         }
                         $count = $db->num_rows($userdata_query);
