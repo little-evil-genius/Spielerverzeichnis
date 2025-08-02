052502ea08e96e874f7e40351cb363f34fc0fc88
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-08-02 14:14:54 +0200

    Update playerdirectory.php

--- inc/plugins/playerdirectory.php
+++ inc/plugins/playerdirectory.php
@@ -1618,7 +1618,10 @@ function playerdirectory_admin_update_plugin(&$table) {
                 $table_status = $db->fetch_array($query);
                 $actual_collation = strtolower($table_status['Collation'] ?? '');
 
-                if (!empty($collation) && $actual_collation !== strtolower($collation)) {
+                $actual_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $actual_collation);
+                $expected_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $collation);
+
+                if (!empty($collation) && $actual_collation !== $expected_collation) {
                     $db->query("ALTER TABLE {$table} CONVERT TO CHARACTER SET {$charset} COLLATE {$collation}");
                 }
             }
@@ -7595,8 +7598,11 @@ function playerdirectory_is_updated() {
         ");
         $result = $db->fetch_array($query);
         $actual_collation = strtolower($result['TABLE_COLLATION'] ?? '');
+        
+        $actual_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $actual_collation);
+        $expected_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $collation);
 
-        if ($actual_collation !== $collation) {
+        if ($actual_collation !== $expected_collation) {
             return false;
         }
     }
